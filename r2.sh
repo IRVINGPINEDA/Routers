13568d6bac9277adc6d8b4a3add18f3e5278771a
Irving <161105017+IRVINGPINEDA@users.noreply.github.com>
2025-10-23 11:27:49 -0600

    Update r2.sh

--- r2.sh
+++ r2.sh
@@ -14,14 +14,14 @@ network:
     enp0s9:
       addresses: [192.168.0.5/30]    # Hacia R2
 EOF
-sudo netplan apply
+netplan apply
 
 # 2. Habilitar IP forwarding
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf
+sysctl -p
 
 # 3. Configurar FRR
-sudo cat > /etc/frr/frr.conf << EOF
+cat > /etc/frr/frr.conf << EOF
 hostname R1
 !
 interface enp0s3
@@ -41,5 +41,5 @@ line vty
 EOF
 
 # 4. Reiniciar FRR
-sudo systemctl restart frr
+systemctl restart frr
 echo "R1 configurado correctamente"
